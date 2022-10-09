d2546a17acea1bb378b05ab5d5dc5e4c1bddca7d
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-10-09 16:34:11 -0300

    feat: create page nicksnames

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,50 @@
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <div className="h-screen bg-[url('/bg.png')] bg-no-repeat bg-cover flex items-center justify-center ">
+      <form className="flex flex-col space-y-6 items-end">
+        <img src="/logo.png" alt="Logo DevFire" />
+
+        <div className="flex space-x-4 w-full justify-end w-480 ">
+          <label
+            htmlFor="playOne"
+            className="font-pixel text-shadow text-red-black text-6xl uppercase text-stroke font-bold"
+          >
+            p1
+          </label>
+          <input
+            id="playOne"
+            type="text"
+            maxLength={10}
+            className="rounded-full px-5 font-pixel text-4xl bg-yellow-100 shadow-inset"
+            autoComplete="off"
+          ></input>
+        </div>
+
+        <div className="flex space-x-4 w-full justify-end">
+          <label
+            htmlFor="playTwo"
+            className="font-pixel text-shadow text-blue-black text-6xl uppercase text-stroke font-bold"
+          >
+            p2
+          </label>
+          <input
+            id="playTwo"
+            type="text"
+            maxLength={10}
+            className="rounded-full px-5 font-pixel text-4xl bg-yellow-100 shadow-inset"
+            autoComplete="off"
+          ></input>
+        </div>
+        <div className="flex items-center justify-center w-full">
+          <button
+            type="submit"
+            className="bg-pink-100 text-white font-pixel text-5xl px-12 py-3 rounded-full shadow-2xl uppercase"
+          >
+            play
+          </button>
+        </div>
+      </form>
+    </div>
   )
 }
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,27 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       colors: {
+        'red-black': '#ff002a',
+        'blue-black': '#388cfe',
+        yellow: {
+          100: '#FAE319'
+        },
+        green: {
+          200: '#4DBA47'
+        },
+        pink: {
+          100: '#FF29DC'
+        }
+      },
+      fontFamily: {
+        pixel: ['VT323', 'monospace']
+      },
+      boxShadow: {
+        inset: 'inset 0px 4px 4px rgba(0, 0, 0, 0.25);'
+      }
+    },
   },
   plugins: [],
 }
