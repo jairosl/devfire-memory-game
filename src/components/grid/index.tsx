49268b58b5aa9192bc38a28a196e0f81b163baf7
jairosl <jairo.lima@aluno.uepb.edu.br>
2022-11-25 21:53:42 -0300

    feat: create page start game

--- src/components/grid/index.tsx
+++ src/components/grid/index.tsx
@@ -1,27 +1,51 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import { Card, ICard } from "../card"
 import { regenerateCard } from "../../util/card-util"
 import "./styles.css"
+import { usePlay } from "../../contexts/play"
+import { useNavigate } from "react-router-dom"
 export interface IGrid {
   cards: ICard[]
 }
 export function Grid({ cards }: IGrid) {
+  const { nicknamePlayerOne, nicknamePlayerTwo } = usePlay()
+  const navigation = useNavigate()
+
   const [stateCards, setStateCards] = useState(() => {
     return regenerateCard(cards)
   })
+
   // estados de cards virados
   const firstCardTurned = useRef<ICard | null>(null)
   const secondCardTurned = useRef<ICard | null>(null)
+
   // contador de movimentos
   const [moves, setMoves] = useState(0)
   const [p1score, setp1Score] = useState(0)
   const [p2score, setp2Score] = useState(0)
+
   const handleReset = () => {
     setStateCards(regenerateCard(cards))
     setMoves(0)
     setp1Score(0)
     setp2Score(0)
   }
+
+  const handleWinner = () => {
+    if (p1score >= 7) {
+      alert(`O Vencedor é ${nicknamePlayerOne}`)
+    } else {
+      alert(`O Vencedor é ${nicknamePlayerTwo}`)
+    }
+
+    navigation("/")
+  }
+
+  useEffect(() => {
+    if (p1score >= 7 || p2score >= 7) {
+      handleWinner()
+    }
+  }, [p1score, p2score])
 
   const handleClick = (id: string) => {
     const newStateCards = stateCards.map((card) => {
@@ -55,13 +79,13 @@ export function Grid({ cards }: IGrid) {
           setMoves((m) => m + 1)
         } else {
           setTimeout(() => {
-            if (firstCardTurned.current != null && secondCardTurned.current != null) {
+            if (firstCardTurned.current !== null && secondCardTurned.current !== null) {
               firstCardTurned.current.flipped = false
               firstCardTurned.current = null
               secondCardTurned.current.flipped = false
               secondCardTurned.current = null
             }
-          }, 300)
+          }, 10)
         }
         setMoves((m) => m + 1)
       }
@@ -73,18 +97,39 @@ export function Grid({ cards }: IGrid) {
 
   return (
     <>
-      <div className="Scores">
-        <h1>
-          <p>Player 1: {p1score}</p>
-          <p>Player 2: {p2score}</p>
-          <p>Movimentos: {moves}</p>
-        </h1>
-        <button onClick={handleReset}> Resetar/Reembaralhar </button>
-      </div>
-      <div className="grid">
-        {stateCards.map((card) => {
-          return <Card {...card} key={card.id} handleClick={handleClick} />
-        })}
+      <div className="h-screen bg-[url('/bg.png')] bg-no-repeat bg-cover flex items-center justify-start flex-col ">
+        <h2 className="flex w-1/2 justify-between mt-10 mb-7">
+          <p
+            className={`font-pixel text-red-500 text-4xl  uppercase font-bold ${
+              moves % 2 === 0 ? "text-shadow-white" : ""
+            }`}
+          >
+            {nicknamePlayerOne}: {p1score}
+          </p>
+          <p className="font-pixel t text-white text-4xl uppercase font-bold">
+            Movimentos: {moves}
+          </p>
+          <p
+            className={`font-pixel text-blue-500 text-4xl uppercase font-bold ${
+              moves % 2 === 1 ? "text-shadow-white" : ""
+            }`}
+          >
+            {nicknamePlayerTwo}: {p2score}
+          </p>
+        </h2>
+
+        <div className="grid">
+          {stateCards.map((card) => {
+            return <Card {...card} key={card.id} handleClick={handleClick} />
+          })}
+        </div>
+
+        <button
+          className="bg-pink-100 text-white font-pixel text-3xl px-6 py-2 rounded-full shadow-2xl uppercase mt-6"
+          onClick={handleReset}
+        >
+          Resetar/Reembaralhar
+        </button>
       </div>
     </>
   )
